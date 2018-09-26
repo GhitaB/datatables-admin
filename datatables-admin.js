01a1f9c0b82db942d66cc7affc99489dd7257488
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-09-26 06:43:30 +0300

    Add controls, improve design.

--- datatables-admin.js
+++ datatables-admin.js
@@ -1,6 +1,6 @@
 new Vue({
   el: '#datatables-admin',
   data: {
-    columns: ['Demo column 1', 'Demo column 2']
+    columns: ['Demo column 1', 'Demo column 2', 'Demo column 3', 'Demo column 4']
   }
 })
