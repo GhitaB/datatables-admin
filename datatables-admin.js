55ac38a808486466749d02a701625816cbd981eb
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-09-26 06:56:16 +0300

    Columns, rows, listing.

--- datatables-admin.js
+++ datatables-admin.js
@@ -1,6 +1,21 @@
 new Vue({
   el: '#datatables-admin',
   data: {
-    columns: ['Demo column 1', 'Demo column 2', 'Demo column 3', 'Demo column 4']
+    columns: ['Demo column 1', 'Demo column 2', 'Demo column 3', 'Demo column 4'],
+    rows: [
+      {
+        'Demo column 1': 'some data here',
+        'Demo column 2': 'test',
+        'Demo column 3': 'test 2',
+        'Demo column 4': 'something'
+      },
+
+      {
+        'Demo column 1': 'second row',
+        'Demo column 2': 'test',
+        'Demo column 3': 'test 2',
+        'Demo column 4': 'something'
+      }
+    ]
   }
 })
